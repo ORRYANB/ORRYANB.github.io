fde12fa4d53d1900b3c90dfe33e17617517800ce
Ori Baber <ori.baber@gmail.com>
2021-01-06 16:37:25 -0500

    Update config.js

--- health-vignettes/health-gainesville/config.js
+++ health-vignettes/health-gainesville/config.js
@@ -342,7 +342,7 @@ var config = {
           id: 'chapter-7',
           title: ' ',
           image: '',
-          description: 'gainesville vpa grey',
+          description: 'gainesville vpa',
           location: {
             center: [-82.32722, 29.65415],
             zoom: 11.83,
@@ -397,30 +397,14 @@ var config = {
           id: 'chapter-8',
           title: ' ',
           image: '',
-          description: 'street lights',
+          description: 'gainesville vpa',
           location: {
-            center: [-82.32517, 29.65860],
-            zoom: 10.92,
-            pitch: 0.00,
-            bearing: -0.35
+            center: [-82.32722, 29.65415],
+            zoom: 11.83,
+            pitch: 48.50,
+            bearing: 0.00
           },
           onChapterEnter: [
-              {
-                layer: 'health-alachua',
-                opacity: 1
-              },
-              {
-                layer: 'uf',
-                opacity: 0
-              },
-              {
-                layer: 'gainesville-cras',
-                opacity: 0
-              },
-              {
-                layer: 'i75',
-                opacity: 0
-              },
               {
                 layer: 'black-dot-2010',
                 opacity: 0
@@ -430,78 +414,39 @@ var config = {
                 opacity: 0
               },
               {
-                layer: 'vpa-county-color',
+                layer: 'i75',
                 opacity: 0
+              },
+              {
+                layer: 'street-lights',
+                opacity: 0
+              },
+              {
+                layer: 'street-light-mesh',
+                opacity: 0
+              },
+              {
+                layer: 'gainesville-cras',
+                opacity: 0
+              },
+              {
+                layer: 'vpa-county-color',
+                opacity: 1
               },
               {
                 layer: 'vpa-county-grey',
                 opacity: 0
               },
               {
-                layer: 'street-lights',
-                opacity: 1
+                layer: 'health-alachua',
+                opacity: 0
               },
               {
-                layer: 'street-light-mesh',
-                opacity: 1
+                layer: 'uf',
+                opacity: 0
               }
           ],
           onChapterExit: []
-      },
-      {
-          id: 'chapter-9',
-          title: ' ',
-          image: '',
-          description: 'street lights mesh',
-          location: {
-            center: [-82.32517, 29.65860],
-            zoom: 10.92,
-            pitch: 0.00,
-            bearing: -0.35
-          },
-          onChapterEnter: [
-              {
-                layer: 'health-alachua',
-                opacity: 1
-              },
-              {
-                layer: 'uf',
-                opacity: 0
-              },
-              {
-                layer: 'gainesville-cras',
-                opacity: 0
-              },
-              {
-                layer: 'i75',
-                opacity: 0
-              },
-              {
-                layer: 'black-dot-2010',
-                opacity: 0
-              },
-              {
-                layer: 'white-dot-2010',
-                opacity: 0
-              },
-              {
-                layer: 'vpa-county-color',
-                opacity: 0
-              },
-              {
-                layer: 'vpa-county-grey',
-                opacity: 0
-              },
-              {
-                layer: 'street-lights',
-                opacity: 1
-              },
-              {
-                layer: 'street-light-mesh',
-                opacity: 1
-              }
-            ],
-            onChapterExit: []
-          }
+      }
     ]
 };
